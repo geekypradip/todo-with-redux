c9edf77167142be428c138537bb0be16e5c7ccae
Pradip Mandal <54275322+geekypradip@users.noreply.github.com>
2022-01-08 21:01:05 +0530

    todo

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,7 @@
-import Counter from './counter';
+
 import Todo from './todo';
 import { combineReducers } from 'redux';
 const allreducers = combineReducers({
-    Count: Counter,
     Todos: Todo
 })
 export default allreducers;
