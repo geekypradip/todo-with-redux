b504b0ac480ad8ce2f843774588ddffb3f23492a
Pradip Mandal <54275322+geekypradip@users.noreply.github.com>
2022-01-08 21:06:26 +0530

    Update index.js

--- src/action/index.js
+++ src/action/index.js
@@ -1,28 +1,5 @@
 import { v4 as uuidv4 } from 'uuid';
-export const increment = (val) => {
-    return {
-        type: "INCREMENT",
-        payload: val
-    }
-};
-export const decrement = (val) => {
-    return {
-        type: "DECREMENT",
-        payload: val
-    }
-};
-export const multiply = (val) => {
-    return {
-        type: "MULTIPLY",
-        payload: val
-    }
-};
-export const division = (val) => {
-    return {
-        type: "DIVISION",
-        payload: val
-    }
-};
+
 export const add = ({ text, desc }) => {
     return {
         type: "ADD",
